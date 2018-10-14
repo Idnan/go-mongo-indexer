cde878bf76d1506584d63a649bdb2766fc9ab8b8
Adnan Ahmed <idnan.se@gmail.com>
2018-10-14 22:16:08 +0400

    Few more comments

--- pkg/util/main.go
+++ pkg/util/main.go
@@ -5,25 +5,30 @@ import (
 	"fmt"
 )
 
-func StringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
+// Find a given string in an array of strings
+func StringInSlice(needle string, haystack []string) bool {
+	for _, elem := range haystack {
+		if elem == needle {
 			return true
 		}
 	}
+
 	return false
 }
 
+// Converts data to json string
 func JsonEncode(data interface{}) string {
 	b, _ := json.Marshal(data)
 
 	return string(b)
 }
 
+// Print to stdout with green color
 func PrintGreen(text string) {
 	fmt.Printf("\033[1;32m%s\033[0m", text)
 }
 
+// Print to stdout with red color
 func PrintRed(text string) {
 	fmt.Printf("\033[1;31m%s\033[0m", text)
 }
